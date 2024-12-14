9dfceebc4a390644e4675b4bfea1c8ea6056eeb0
mridulchopraX <mridulchopra444@gmail.com>
2024-12-14 20:41:33 +0000

    Add the container startup in command

--- run-command.sh
+++ run-command.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+script_dir="$(dirname "$(realpath "$0")")"
+IMAGE_NAME="isolated-git"
+CONTAINER_NAME="$username-git"
+SSH_DIR="$script_dir/internal/$username/.ssh"
+GNUPG_DIR="$script_dir/internal/$username/.gnupg"
+PROJECT_HOME="$HOME/Documents/mridulchopraX"
+
+
 CONTAINER_NAME="$1-git"
 COMMAND=$2
 
@@ -12,6 +20,23 @@ for arg in "${ARGS[@]}"; do
     ARGS_STR+=$arg
 done
 ARGS_STR=${ARGS_STR% } # Remove the trailing space
+
+
+if [ ! "$(docker ps -a -q -f name=^${CONTAINER_NAME}$)" ]; then
+    echo "📦 Creating and starting container '$CONTAINER_NAME'..."
+    docker run -dti \
+    -v $SSH_DIR:"/root/.ssh" \
+    -v $GNUPG_DIR:"/root/.gnupg" \
+    -v "$PROJECT_HOME":"/projects" \
+    --name "$CONTAINER_NAME" \
+    "$IMAGE_NAME" \
+    /bin/ash
+elif [ ! "$(docker ps -q -f name=^${CONTAINER_NAME}$)" ]; then
+    # Container exists but is not running
+    echo "⚙️ Starting the existing container '$CONTAINER_NAME'."
+    docker start "$CONTAINER_NAME"
+fi
+
 
 
 COMMAND_STR+="\"$COMMAND\" "
